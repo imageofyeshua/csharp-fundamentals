15686736f694283402939bc8e13b37187af8ba64
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-09 08:28:10 +0900

    Selection Statement

---
+++ SelectionStatements/Animals.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace SelectionStatements;
+
+class Animal
+{
+  public string? Name;
+  public DateTime Born;
+  public byte Legs;
+}
+
+class Cat : Animal
+{
+  public bool IsDomestic;
+}
+
+class Spider : Animal
+{
+  public bool IsVenomous;
+}
+
+
