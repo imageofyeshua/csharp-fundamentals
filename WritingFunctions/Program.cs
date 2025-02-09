15352a3da29e239300f1701675d423b6faca7097
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-10 08:27:25 +0900

    Finish Writing Functions

---
+++ WritingFunctions/Program.cs
@@ -0,0 +1,8 @@
+﻿// TimesTable(number: 4, size: 20);
+ConfigureConsole(culture: "fr-FR");
+decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+WriteLine($"You must pay {taxToPay:C} in tax.");
+
+RunCardinalToOrdinal();
+RunFactorial();
+RunFibImperative();
