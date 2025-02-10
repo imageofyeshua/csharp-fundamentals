25f26a07cddec94b0a921f9800e1d4e8be1d231f
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-10 09:10:07 +0900

    Finish Debugging

---
+++ Debugging/Program.cs
@@ -0,0 +1,14 @@
+﻿Write("Enter a number: ");
+string number = ReadLine()!;
+double a = double.Parse(number);
+
+double Add(double a, double b)
+{
+  return a + b; // deliberate bug!
+}
+
+double b = 2.5;
+double answer = Add(a, b);
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press Enter to end the app.");
+ReadLine();
