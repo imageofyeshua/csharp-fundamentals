1ad1997906726b48a686863ae859c3bc19d9438a
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-10 22:17:13 +0900

    CallStack Exception Handling

---
+++ CallStackExceptionHandling/Program.cs
@@ -0,0 +1,21 @@
+﻿using CallStackExceptionHandlingLib;
+WriteLine("In Main");
+Alpha();
+void Alpha()
+{
+  WriteLine("In Alpha");
+  Beta();
+}
+void Beta()
+{
+  WriteLine("In Beta");
+  try
+  {
+    Processor.Gamma();
+  }
+  catch (Exception ex)
+  {
+    WriteLine($"Caught this: {ex.Message}");
+    throw;
+  }
+}
