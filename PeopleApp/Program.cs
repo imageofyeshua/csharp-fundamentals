a8522ab1fb14a8620bc6317ca577b628c107c3c9
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-11 08:47:04 +0900

    BankAccount Static Example

--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Packt.Shared;
+﻿using System.Globalization;
+using Packt.Shared;
 
 ConfigureConsole();
 
@@ -27,3 +28,42 @@ Person alice = new()
   Born = new(1998, 3, 7, 16, 28, 0, TimeSpan.Zero)
 };
 WriteLine(format: "{0} was born on {1:d}.", arg0: alice.Name, arg1: alice.Born);
+
+Person alfred = new Person();
+// Works with all versions of C#.
+alfred.Name = "Alfred";
+bob.Children.Add(alfred);
+// Works with C# 3 and later.
+bob.Children.Add(new Person { Name = "Bella" });
+// Works with C# 9 and later.
+bob.Children.Add(new() { Name = "Zoe" });
+WriteLine($"{bob.Name} has {bob.Children.Count} children");
+
+for (int childIndex = 0; childIndex < bob.Children.Count; childIndex++)
+{
+  WriteLine($"> {bob.Children[childIndex].Name}");
+}
+
+foreach (Person child in bob.Children)
+{
+  WriteLine($">> {child.Name}");
+}
+
+BankAccount.InterestRate = 0.012M; // Store a shared value in static field.
+BankAccount jonesAccount = new();
+jonesAccount.AccountName = "Mrs. Jones";
+jonesAccount.Balance = 2400;
+WriteLine(
+  format: "{0} earned {1:C} interest.",
+  arg0: jonesAccount.AccountName,
+  arg1: jonesAccount.Balance * BankAccount.InterestRate
+);
+
+BankAccount danielAccount = new();
+danielAccount.AccountName = "Mr. Daniel";
+danielAccount.Balance = 10_000_000;
+WriteLine(
+  format: "{0} earned {1:C} interest.",
+  arg0: danielAccount.AccountName,
+  arg1: danielAccount.Balance * BankAccount.InterestRate
+);
