b339e85421e017cc5847a716940f0792b0926999
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-13 17:41:32 +0900

    Packaging to Publish

---
+++ AotConsole/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Globalization;
+
+WriteLine("This is an ahead-of-time (AOT) compiled console app.");
+WriteLine("Current culture: {0}", CultureInfo.CurrentCulture.DisplayName);
+WriteLine("OS version: {0}", Environment.OSVersion);
+Write("End of the old World");
