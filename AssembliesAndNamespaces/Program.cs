9f736ab7a99536929eae7fd20242d76ac260142f
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-13 15:24:45 +0900

    Environment Check

--- AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/Program.cs
@@ -5,3 +5,8 @@ XDocument doc = new();
 string s1 = "Hello";
 string s2 = "World";
 WriteLine($"{s1} {s2}");
+
+WriteLine($"Environment.Is64BitProcess = {Environment.Is64BitProcess}");
+WriteLine($"Environment.Is64BitOperatingSystem = {Environment.Is64BitOperatingSystem}");
+WriteLine($"int.MaxValue = {int.MaxValue:N0}");
+WriteLine($"nint.MaxValue = {nint.MaxValue:N0}");
