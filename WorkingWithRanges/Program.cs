abaa84df28ac295e69cc648a1ffc84bc03460e83
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-15 23:00:30 +0900

    Finish Working with Range

--- WorkingWithRanges/Program.cs
+++ WorkingWithRanges/Program.cs
@@ -18,3 +18,26 @@ string lastName = name.Substring(
 WriteLine($"First: {firstName}, Last: {lastName}");
 
 // Using Spans.
+ReadOnlySpan<char> namesAsSpan = name.AsSpan();
+ReadOnlySpan<char> firstNameSpan = namesAsSpan[0..lengthOfFirst];
+ReadOnlySpan<char> lastNameSpan = namesAsSpan[^lengthOfLast..];
+
+WriteLine($"First: {firstName}, Last: {lastName}");
+
+ReadOnlySpan<char> text = "12+23+456".AsSpan();
+int sum = 0;
+foreach (Range r in text.Split('+'))
+{
+  sum += int.Parse(text[r]);
+}
+WriteLine($"Sum using Split: {sum}");
+
+WriteLine($"First: {firstName}, Last: {lastName}");
+
+ReadOnlySpan<char> multi = "3*12*43".AsSpan();
+int result = 1;
+foreach (Range r in multi.Split('*'))
+{
+  result *= int.Parse(multi[r]);
+}
+WriteLine($"Result using Split: {result}");
