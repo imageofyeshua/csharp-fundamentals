91b65d8bb15b169359957ef96bc5618686adb628
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-20 10:33:59 +0900

    Rule

---
+++ SpectreConsole/Program.Rule.cs
@@ -0,0 +1,34 @@
+using Spectre.Console;
+
+partial class Program
+{
+  public static void Rule()
+  {
+    // Usage.
+    var rule = new Rule();
+    AnsiConsole.Write(rule);
+
+    // Title.
+    var ruleTwo = new Rule("[red]Hello[/]");
+    AnsiConsole.Write(ruleTwo);
+
+    // Title alignment.
+    var ruleThree = new Rule("[red]Hello[/]");
+    ruleThree.Justification = Justify.Left;
+    AnsiConsole.Write(ruleThree);
+
+    var ruleFour = new Rule("[red]Hello[/]");
+    ruleFour.LeftJustified();
+    AnsiConsole.Write(ruleFour);
+
+    // Styling.
+    var ruleFive = new Rule("[red]Hello[/]");
+    ruleFive.Style = Style.Parse("red dim");
+    AnsiConsole.Write(ruleFive);
+
+    var ruleSix = new Rule("[red]Hello[/]");
+    ruleSix.RuleStyle("red dim");
+    AnsiConsole.Write(ruleSix);
+
+  }
+}
--- SpectreConsole/Program.cs
+++ SpectreConsole/Program.cs
@@ -4,9 +4,9 @@ public static partial class Program
 {
   public static void Main(string[] args)
   {
-    Columns();
+    // Columns();
 
-    Table();
+    // Table();
 
     // Tree();
 
@@ -16,6 +16,7 @@ public static partial class Program
 
     // BreakdownChart();
 
+    Rule();
   }
 }
 
